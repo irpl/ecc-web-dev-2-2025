3cd76063a1376d06b1799ccaedf74d59fccd552d
Phillip Logan <philo3194@gmail.com>
2025-11-14 12:07:59 -0500

    added click counter code

---
+++ ClickCounterViewState/ClickCounterViewState/Default.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ClickCounterViewState.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button Text="Click me!" runat="server" OnClick="CountClick"/>
+            <asp:Label ID="lblCount" Text="0" runat="server" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ClickCounterViewState/ClickCounterViewState/Default.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ClickCounterViewState
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (ViewState["count"] != null) {
+                lblCount.Text = ViewState["count"].ToString();
+            }
+        }
+
+        //protected void ChangeColor(object sender, EventArgs e)
+        //{
+        //    pnl.BackColor = System.Drawing.Color.Green;
+        //}
+
+        protected void CountClick(object sender, EventArgs e)
+        {
+            if (ViewState["count"] == null)
+            {
+                ViewState["count"] = 1;
+                lblCount.Text = ViewState["count"].ToString();
+            }
+            else {
+                ViewState["count"] = (int)ViewState["count"] + 1;
+                lblCount.Text = ViewState["count"].ToString();
+            }
+        }
+
+    }
+}
