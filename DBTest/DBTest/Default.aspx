aafbf7beda2c364ca6e298e913499d3f4d795df5
Phillip Logan <philo3194@gmail.com>
2025-12-04 09:15:55 -0500

    db test

---
+++ DBTest/DBTest/Default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="DBTest.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="btnTest" runat="server" 
+                        Text="Test Database Connection" 
+                        OnClick="btnTest_Click" />
+            <br /><br />
+            <asp:Label ID="lblResult" runat="server" Font-Size="Large"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ DBTest/DBTest/Default.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+
+namespace DBTest
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnTest_Click(object sender, EventArgs e)
+        {
+            string connString = ConfigurationManager
+                .ConnectionStrings["Web2DbConnection"].ConnectionString;
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connString))
+                {
+                    conn.Open();
+
+                    using (SqlCommand cmd = new SqlCommand("SELECT GETDATE();", conn))
+                    {
+                        object result = cmd.ExecuteScalar();
+                        lblResult.Text = "✅ Connection OK. Server time: " + result.ToString();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                lblResult.Text = "❌ Connection FAILED: " + ex.Message;
+            }
+        }
+
+    }
+}
